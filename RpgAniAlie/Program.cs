8a80e83eb5429efdc4f63deea40f8f01a77a7d58
Guilherme Lima Braga <guimaster90@gmail.com>
2020-02-18 18:37:41 -0300

    Teste.

---
+++ RpgAniAlie/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RpgAniAlie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste");
+            Console.ReadKey();
+        }
+    }
+}
