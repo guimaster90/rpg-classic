6866c245407cfc1c229fd82aaca1fdc2aaa64a69
Rodrigo Franco <rodrigocsfranco@gmail.com>
2020-05-29 17:25:20 -0300

    Adicionado Classes para os equipamento e a criação dos  sets de armadura como objetos de suas respectivas classes.

---
+++ RpgAniAlie/Equipamento/Bota.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgAniAlie.Equipamento
+{
+    public class Bota : Equip
+    {
+
+
+    }
+}
---
+++ RpgAniAlie/Equipamento/Capacete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgAniAlie.Equipamento
+{
+    public class Capacete : Equip
+    {
+        
+    }
+}
