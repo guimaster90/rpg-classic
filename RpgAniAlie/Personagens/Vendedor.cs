31312b8aef166bef6d3baf65c8e2a0032ced29c0
Guilherme Lima Braga <guimaster90@gmail.com>
2020-06-11 16:58:52 -0300

    .

--- RpgAniAlie/Personagens/Vendedor.cs
+++ RpgAniAlie/Personagens/Vendedor.cs
@@ -10,18 +10,18 @@ namespace RpgAniAlie.Personagens
 
         public void VenderPocao()
         {
-            Console.WriteLine("Você quer comprar poção?");
+            
             string resposta = Console.ReadLine();
             if (resposta.Equals("Sim") || resposta.Equals("sim"))
             {
-                Console.WriteLine("Você quer quantas poções?");
+                
                 string qtd = Console.ReadLine();
                 int quantos = Int32.Parse(qtd);
                 if (Moeda.QuantidadeMoeda < quantos)
                 {
-                    Console.WriteLine("Você n tem moedas pra comprar poção");
+                    
                 } else {
-                    Console.WriteLine("Compra efetuada com sucesso");
+                    
                     Pocao.qtdPocao += quantos;
                 }
 
